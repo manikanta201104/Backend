36e387b79ef0e19a77708ceefa1d1db648833ece
manikanta9809 <varshinivarma16@gmail.com>
2025-01-14 11:13:17 +0530

    Added Inital files

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/about', (req, res) => {
+    res.send('This is about page');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('This is contact page');
+});
+
+app.get('/kanna', (req, res) => {
+    res.send("<h1>This is Manikanta from kanna route</h1>");
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        console.error(`Failed to start server: ${err.message}`);
+    } else {
+        console.log(`Server is running on port ${port}`);
+    }
+});
